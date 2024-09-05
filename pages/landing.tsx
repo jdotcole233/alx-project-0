5d0bd588af542b3bb85f903da0dd902389d61fb8
Jay Cole <jdotcole233@gmail.com>
2024-09-05 12:34:39 -0500

    first commit

---
+++ pages/landing.tsx
@@ -0,0 +1,12 @@
+import Card from "@/components/card";
+
+const Landing: React.FC =  () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default Landing
